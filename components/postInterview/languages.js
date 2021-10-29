f81e7f9082391ee13432a1da7ced0ab8194b662f
eric saldivar <esaldivar1214@gmail.com>
2021-10-28 20:32:03 -0500

    added checkboxes to the languages

--- components/postInterview/languages.js
+++ components/postInterview/languages.js
@@ -8,35 +8,60 @@ export default function Languages() {
         <div className="ml-10">
           <label className="flex items-center p-2">
             <input
-              className="mr-2"
-              type="radio"
+              type="checkbox"
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
               name="Javascript"
               value="Javascript"
             />
             <span>Javascript</span>
           </label>
           <label className="flex items-center p-2">
-            <input className="mr-2" type="radio" name="Python" value="Python" />
+            <input
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
+              type="checkbox"
+              name="Python"
+              value="Python"
+            />
             <span>Python</span>
           </label>
         </div>
         <div className="ml-10">
           <label className="flex items-center p-2">
-            <input className="mr-2" type="radio" name="Java" value="Java" />
+            <input
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
+              type="checkbox"
+              name="Java"
+              value="Java"
+            />
             <span>Java</span>
           </label>
           <label className="flex items-center p-2">
-            <input className="mr-2" type="radio" name="C++" value="C++" />
+            <input
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
+              type="checkbox"
+              name="C++"
+              value="C++"
+            />
             <span>C++</span>
           </label>
         </div>
         <div className="ml-10">
           <label className="flex items-center p-2">
-            <input className="mr-2" type="radio" name="Ruby" value="Ruby" />
+            <input
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
+              type="checkbox"
+              name="Ruby"
+              value="Ruby"
+            />
             <span>Ruby</span>
           </label>
           <label className="flex items-center p-2">
-            <input className="mr-2" type="radio" name="Go" value="Go" />
+            <input
+              className="mr-2 appearance-none checked:bg-blue-600 checked:border-transparent"
+              type="checkbox"
+              name="Go"
+              value="Go"
+            />
             <span>Go</span>
           </label>
         </div>
