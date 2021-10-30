ba9425e30e74a0bff87cff9f8fa872bcab16e19f
cwalls45 <cwalls45@gmail.com>
2021-10-30 18:02:00 -0400

    no change

--- components/navbar/SearchBar.jsx
+++ components/navbar/SearchBar.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
-import SearchSVG from "./searchSVG";
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { reviewActionCreator } from '../../redux';
+import SearchSVG from "./searchSVG";
 
 export default function SearchBar() {
+
     const reviews = useSelector((state) => state.reviews)
     const dispatch = useDispatch()
     const { updateReviews } = bindActionCreators(reviewActionCreator, dispatch);
+
+    const router = useRouter();
 
     const [formValue, setFormValue] = useState('');
 
@@ -17,7 +21,8 @@ export default function SearchBar() {
         const reviews = await axios.get('/api/getCompanyReviews', {
             params: { company: formValue }
         })
-        updateReviews(reviews.data.reviews)
+        updateReviews(reviews.data.reviews);
+        router.push('/reviewsList');
         return;
     }
 
@@ -32,6 +37,7 @@ export default function SearchBar() {
                 className='h-12 px-5 text-lg rounded-l-lg outline-none w-96'
                 placeholder='Search Companies'
                 onChange={(e) => setFormValue(e.target.value)}
+                onKeyDown={onEnterButton}
                 value={formValue}
             />
             <button className='flex items-center justify-center w-16 h-12 bg-white rounded-r-lg' onClick={getReviews}>
