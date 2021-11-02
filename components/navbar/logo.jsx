f7cffc38a2e5250f4a05b6fc0bf188943da72b10
mikloska <mikloska973@gmail.com>
2021-11-01 21:48:04 -0400

    Added favicon and icon to navbar

--- components/navbar/logo.jsx
+++ components/navbar/logo.jsx
@@ -1,9 +1,11 @@
 import NavButton from "./navButton";
+import Link from 'next/link';
 
 export default function Logo() {
     return (
         <div className='m-2'>
-            <NavButton text='IP' route='/' />
+          {/* <NavButton text='IP' route='/' /> */}
+          <Link href='/'><img className='w-12 cursor-pointer ml-7' src='/Wigglytuff.png'/></Link>
         </div>
     );
 }
